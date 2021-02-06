dbe89ab88676f962e0cde3b3c13f3a6a574b5f10
Dmitriy Marchenko <dmma.job@gmail.com>
2021-02-06 19:14:48 +0200

    Missed more than one symbols

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,8 @@ public class Main {
             System.out.println("Input a value: ");
             int number = in.nextInt();
             if (number < 0) System.out.println("Forbidden! Entered number is negative");
-            if (number >= 0 && number < 17) System.out.println("Entered number part of interval 0 - 17");
-            if (number > 18 && number < 65) System.out.println("Entered number part of interval 18 - 65");
+            if (number >= 0 && number <= 17) System.out.println("Entered number part of interval 0 - 17");
+            if (number >= 18 && number <= 65) System.out.println("Entered number part of interval 18 - 65");
             if (number >= 66) System.out.println("Entered number part of interval 66 - ∞");
         } catch (InputMismatchException e) {
             System.out.println("Something went wrong.");
